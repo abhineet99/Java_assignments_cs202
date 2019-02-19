8901633b01345cd36dc0f709d2cf8fae2fa6c353
Abhineet Pandey <2017csb1062@iitrpr.ac.in>
2019-02-19 18:23:45 +0530

    uploading files

---
+++ lex-assignment-1/myscanner.h
@@ -0,0 +1,13 @@
+#define TYPE 1
+// #define INT 2
+// #define CHAR 3
+// #define FLOAT 4
+// #define DOUBLE 5
+// #define INT_PTR 5
+// #define CHAR_PTR 7
+// #define FLOAT_PTR 8
+// #define DOUBLE_PTR 9
+#define IDENTIFIER 10
+#define INTEGER 11
+#define DBL_BRACKETS 12
+
